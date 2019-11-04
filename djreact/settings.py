e320bbb72b5b91fd22e2b6db105ca77e854e2e29
raraku <airenov500@gmail.com>
2019-11-04 17:20:02 +0100

    'work2'

--- djreact/settings.py
+++ djreact/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "xil5p)ri@z4lo0pi2jma(cmqi#5uti)7_(=3)np5wd@j2t7@a8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["https://aire-1.herokuapp.com", "*"]
 
