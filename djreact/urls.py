0d4ff07f8f239d6c8d98c5714078342e2e6f71c5
raraku <airenov500@gmail.com>
2019-11-04 16:59:46 +0100

    allllljfijei

--- djreact/urls.py
+++ djreact/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.views.decorators.cache import never_cache
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.decorators.cache import never_cache
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -24,5 +25,5 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
     path("rest-auth/", include("rest_auth.urls")),
     path("api/", include("articles.api.urls")),
-    re_path(".*", TemplateView.as_view(template_name="index.html")),
+    re_path("/", never_cache(TemplateView.as_view(template_name="index.html"))),
 ]
