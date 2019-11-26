00721b3352f348ec31f63be3bb75f692ee0b37ba
Raraku <airenov500@gmail.com>
2019-11-26 10:34:44 +0100

    hacked the database

--- articles/api/views.py
+++ articles/api/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from .serializers import ArticleSerializer
 from articles.models import Article
+from rest_framework.permissions import AllowAny
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
+    permission_classes=[AllowAny,]
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
