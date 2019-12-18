c5930374181226af62ff3d419f8bdbb91ad0639a
Raraku <airenov500@gmail.com>
2019-12-18 13:23:12 +0100

    no-reason

--- articles/models.py
+++ articles/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 
 class Article(models.Model):
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=120, blank=True, null=True)
     content = models.TextField()
     description = models.TextField(blank=True)
