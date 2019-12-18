81008047e3b93f1e0081f39e8f51dcb6ae03cc13
Raraku <airenov500@gmail.com>
2019-12-18 15:22:07 +0100

    moved gzip files

--- djreact/settings.py
+++ djreact/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "xil5p)ri@z4lo0pi2jma(cmqi#5uti)7_(=3)np5wd@j2t7@a8"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["aire-1.herokuapp.com/", "http://127.0.0.1:8000/", "localhost:8000"]
 
@@ -129,7 +129,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "build/static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-SEO_JS_PRERENDER_TOKEN = 'p1LPA1jS0ULtX1UZo0OL'
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
