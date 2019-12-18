ffdf0a9cd79f722033d9fa6fa87753116426818c
Raraku <airenov500@gmail.com>
2019-12-18 13:17:11 +0100

    no-reason

--- articles/models.py
+++ articles/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class Article(models.Model):
+    author = models.ForeignKey(User)
     title = models.CharField(max_length=120, blank=True, null=True)
     content = models.TextField()
     description = models.TextField(blank=True)
@@ -11,3 +13,4 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+   
