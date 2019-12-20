6751a08c416eeed92e849c212ffe80cac70b4878
Raraku <airenov500@gmail.com>
2019-12-20 15:37:37 +0100

    activated service worker and refixed static files

---
+++ staticfiles/precache-manifest.cddc3ac0938b70eafddb385031392bd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aca28c8707995a89b2aa4e1d06200f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70dc9a679dbec62e3aec",
+    "url": "/static/css/2.6ebcfcee.chunk.css"
+  },
+  {
+    "revision": "004f8e05c448f0d48552",
+    "url": "/static/css/main.ee880e47.chunk.css"
+  },
+  {
+    "revision": "70dc9a679dbec62e3aec",
+    "url": "/static/js/2.057adace.chunk.js"
+  },
+  {
+    "revision": "004f8e05c448f0d48552",
+    "url": "/static/js/main.79d9f4b7.chunk.js"
+  },
+  {
+    "revision": "60f05623933eb017ae52",
+    "url": "/static/js/runtime-main.78c084b0.js"
+  },
+  {
+    "revision": "e41cf3020a9878f265929d0454716973",
+    "url": "/static/media/background.e41cf302.svg"
+  }
+]);
