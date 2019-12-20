31fbf0f603138e6a42c24da707d605d76bdce94c
Raraku <airenov500@gmail.com>
2019-12-20 14:54:16 +0100

    activated service worker and fixed files

---
+++ staticfiles/precache-manifest.cba6db32ba2543dc50686bf9028e2535.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2507bdc12712e56e2632bc7a70682ab6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70dc9a679dbec62e3aec",
+    "url": "/static/css/2.6ebcfcee.chunk.css"
+  },
+  {
+    "revision": "baff2e89401ab8b0c35f",
+    "url": "/static/css/main.ee880e47.chunk.css"
+  },
+  {
+    "revision": "70dc9a679dbec62e3aec",
+    "url": "/static/js/2.057adace.chunk.js"
+  },
+  {
+    "revision": "baff2e89401ab8b0c35f",
+    "url": "/static/js/main.6c384483.chunk.js"
+  },
+  {
+    "revision": "60f05623933eb017ae52",
+    "url": "/static/js/runtime-main.78c084b0.js"
+  },
+  {
+    "revision": "e41cf3020a9878f265929d0454716973",
+    "url": "/static/media/background.e41cf302.svg"
+  }
+]);
